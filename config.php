54051605277cefcdc6bdaaf68cbdbac481105e60
Anddrey <anddrey_1@mail.ru>
2016-06-01 03:46:23 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://k70835ln.bget.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://k70835ln.bget.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/k/k70835ln/k70835ln.bget.ru/public_html/catalog/');
+define('DIR_SYSTEM', '/home/k/k70835ln/k70835ln.bget.ru/public_html/system/');
+define('DIR_LANGUAGE', '/home/k/k70835ln/k70835ln.bget.ru/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/k/k70835ln/k70835ln.bget.ru/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/k/k70835ln/k70835ln.bget.ru/public_html/system/config/');
+define('DIR_IMAGE', '/home/k/k70835ln/k70835ln.bget.ru/public_html/image/');
+define('DIR_CACHE', '/home/k/k70835ln/k70835ln.bget.ru/public_html/system/cache/');
+define('DIR_DOWNLOAD', '/home/k/k70835ln/k70835ln.bget.ru/public_html/system/download/');
+define('DIR_UPLOAD', '/home/k/k70835ln/k70835ln.bget.ru/public_html/system/upload/');
+define('DIR_MODIFICATION', '/home/k/k70835ln/k70835ln.bget.ru/public_html/system/modification/');
+define('DIR_LOGS', '/home/k/k70835ln/k70835ln.bget.ru/public_html/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'k70835ln_opct');
+define('DB_PASSWORD', 'RgcSWu^1');
+define('DB_DATABASE', 'k70835ln_opct');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
